cff13821b9df4f13d421dea751b37ca1ec08e152
oussemah <oussemahamouda6@gmail.com>
2024-12-24 11:32:45 +0100

    copy form original repo

---
+++ database/db.js
@@ -0,0 +1,16 @@
+
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) console.log(err);
+  else console.log("MySQL is connected...");
+});
+
+module.exports = connection;
