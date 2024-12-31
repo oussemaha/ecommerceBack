b64b64373dcbab6243bd0e7f5fb34c8af4d04347
Oussema Hamouda <49439788+oussemaha@users.noreply.github.com>
2024-12-31 17:09:50 +0100

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,10 +1,36 @@
 
 const mysql = require("mysql2");
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
 
+const secret_name = "rds!db-6d254137-faed-4c3d-bda6-99d3d67d88bb";
+
+const client = new SecretsManagerClient({
+  region: "us-east-1",
+});
+
+let response;
+
+try {
+  response = await client.send(
+    new GetSecretValueCommand({
+      SecretId: secret_name,
+      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+    })
+  );
+} catch (error) {
+  // For a list of exceptions thrown, see
+  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+  throw error;
+}
+
+const secret = response.SecretString;
 const connection = mysql.createConnection({
   host: "dbecommerce.cbxisoldayv6.us-east-1.rds.amazonaws.com",
-  user: "admin",
-  password: "4wGuGHJE0OgS.aF>heVNgSuP!D9L",
+  user: secret["username"],
+  password: secret["password"],
   database: "ecommerce",
 });
 
